ba106f6338810e108f6dc1eff509ce6f7d21f88a
crazymaster848 <hihello123hijkl@gmail.com>
2025-11-21 03:48:37 +0530

    Update choco.js

--- choco.js
+++ choco.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("chocoBtn");
+    const box = document.getElementById("couponBox");
+
+    btn.addEventListener("click", () => {
+        box.style.display = "block";
+    });
+});
 
